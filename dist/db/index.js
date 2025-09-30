56a6366aeaa0f995c116291fd49875d9138c5880
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-30 10:27:45 +0200

    added /dist to git ignore

--- dist/db/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { PrismaClient } = require('../generated/prisma');
-const prisma = new PrismaClient();
-exports.default = prisma;
