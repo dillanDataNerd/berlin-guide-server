5deaa646d879062069f80b37a3087876b95d89e9
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-30 10:49:25 +0200

    saved the file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
+import { Application } from "express";
 
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
@@ -14,11 +15,10 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN 
-console.log(FRONTEND_URL)
+const FRONTEND_URL = process.env.ORIGIN;
 
 // Middleware configuration
-module.exports = (app) => {
+module.exports = (app: Application) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
@@ -26,7 +26,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: [FRONTEND_URL],
     })
   );
 
